b1a4cfcba9575e6083a78dbcb081fca1e7a4b1a1
Pablo Morales <moralespablogaston@gmail.com>
2017-11-27 20:04:57 -0300

    pir capture ok

--- examples/peripherals/gpio/main/gpio_example_main.c
+++ examples/peripherals/gpio/main/gpio_example_main.c
@@ -31,13 +31,18 @@
  *
  */
 
+/*
 #define GPIO_OUTPUT_IO_0    18
 #define GPIO_OUTPUT_IO_1    19
 #define GPIO_OUTPUT_PIN_SEL  ((1<<GPIO_OUTPUT_IO_0) | (1<<GPIO_OUTPUT_IO_1))
 #define GPIO_INPUT_IO_0     4
 #define GPIO_INPUT_IO_1     5
 #define GPIO_INPUT_PIN_SEL  ((1<<GPIO_INPUT_IO_0) | (1<<GPIO_INPUT_IO_1))
+
+*/
 #define ESP_INTR_FLAG_DEFAULT 0
+#define GPIO_INPUT_IO_0 16
+#define GPIO_INPUT_PIN_SEL  (1<<GPIO_INPUT_IO_0)
 
 static xQueueHandle gpio_evt_queue = NULL;
 
@@ -59,7 +64,20 @@ static void gpio_task_example(void* arg)
 
 void app_main()
 {
-    gpio_config_t io_conf;
+	gpio_config_t io_conf;
+	//interrupt of rising edge
+	io_conf.intr_type = GPIO_PIN_INTR_POSEDGE;
+	//bit mask of the pins, use GPIO4/5 here
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+	//set as input mode
+	io_conf.mode = GPIO_MODE_INPUT;
+	//enable pull-up mode
+	io_conf.pull_up_en = 0;
+	io_conf.pull_down_en = 0;
+	gpio_config(&io_conf);
+
+
+	/*gpio_config_t io_conf;
     //disable interrupt
     io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
     //set as output mode
@@ -82,7 +100,7 @@ void app_main()
     //enable pull-up mode
     io_conf.pull_up_en = 1;
     gpio_config(&io_conf);
-
+	*/
     //change gpio intrrupt type for one pin
     gpio_set_intr_type(GPIO_INPUT_IO_0, GPIO_INTR_ANYEDGE);
 
@@ -96,19 +114,19 @@ void app_main()
     //hook isr handler for specific gpio pin
     gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
     //hook isr handler for specific gpio pin
-    gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void*) GPIO_INPUT_IO_1);
+    //gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void*) GPIO_INPUT_IO_1);
 
     //remove isr handler for gpio number.
-    gpio_isr_handler_remove(GPIO_INPUT_IO_0);
+    //gpio_isr_handler_remove(GPIO_INPUT_IO_0);
     //hook isr handler for specific gpio pin again
-    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
+    //gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
 
     int cnt = 0;
     while(1) {
-        printf("cnt: %d\n", cnt++);
+        printf("corriendo: %d\n", cnt++);
         vTaskDelay(1000 / portTICK_RATE_MS);
-        gpio_set_level(GPIO_OUTPUT_IO_0, cnt % 2);
-        gpio_set_level(GPIO_OUTPUT_IO_1, cnt % 2);
+        //gpio_set_level(GPIO_OUTPUT_IO_0, cnt % 2);
+        //gpio_set_level(GPIO_OUTPUT_IO_1, cnt % 2);
     }
 }
 
